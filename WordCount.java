da24170de2971c95c82966fb381c3bc500e0aac0
gvrp11 <112675251+gvrp11@users.noreply.github.com>
2022-09-01 22:38:03 -0500

    Change HashMap to TreeMap to support descending order
    Change HashMap to TreeMap and add a custom Comparator to support printing in descending order of values

--- WordCount.java
+++ WordCount.java
@@ -1,7 +1,8 @@
 
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WordCount {
 
@@ -23,7 +24,8 @@ public class WordCount {
 		}
 		
 		String[] words = str.split(" ");
-		Map<String, Integer> wordCount = new HashMap<>();
+		
+		Map<String, Integer> wordCount = new TreeMap<String, Integer>();
 
 		for (String word : words) {
 			word = word.toLowerCase();
@@ -35,9 +37,45 @@ public class WordCount {
 			}
 		}
 		
-		for(String w : wordCount.keySet()) {
+		//This prints key-values sorted by key's natural order
+		/*for(String w : wordCount.keySet()) {
 			System.out.println(wordCount.get(w) + " " + w);
+		} */
+		
+		Map<String, Integer> wcm = new TreeMap<String, Integer>(new ValueComparator(wordCount));
+		
+		wcm.putAll(wordCount);
+		
+		//This prints key-values in the descending order of values. If values are same, then key ascending order will be used
+		for(String w : wcm.keySet()) {
+			System.out.println(wcm.get(w) + " " + w);
 		}
 
 	}
+	
+	static class ValueComparator implements Comparator<String>{
+		
+		private Map<String, Integer> map;
+		
+		ValueComparator(Map<String, Integer> map){
+			this.map = map;
+		}
+
+		@Override
+		public int compare(String o1, String o2) {
+			Integer i1 = map.get(o1);
+			Integer i2 = map.get(o2);
+			
+			int valueComp = i2.compareTo(i1);
+			int keyComp = o1.compareTo(o2);
+			
+			if(valueComp != 0) {
+				return valueComp;
+			}else {
+				return keyComp;
+			}
+		}
+		
+	}
+	 
 }
